1beef3785dee26ddfbcdc81e7c182e24010663a5
Ravi Ghaghada <41049832+RaviGhaghada@users.noreply.github.com>
2018-10-18 20:30:36 +0100

    Added function for calculating combinations

--- Counting.java
+++ Counting.java
@@ -3,13 +3,13 @@
  * A class containing methods for various counting functions such as permutations, combinations, etc.
  *
  * @author Ravi Ghaghada ravighaghada28@gmail.com
- * 16/10/2018
+ * 14/10/2018
  */
 public class Counting
 {
     /* 
-        A method that detects whether a given number is negative or not
-    */
+    A method that detects whether a given number is negative or not
+     */
     private static boolean isNegative(int x, String methodname)
     {
         try{
@@ -28,8 +28,8 @@ public class Counting
     }
 
     /*
-        A method that returns the factorial of a number x
-    */
+    A method that returns the factorial of a number x
+     */
     public static int factorial(int x)
     {
         if (isNegative(x, "factorial"))
@@ -43,25 +43,46 @@ public class Counting
         else
             return x*factorial(x-1);
     }
-    
+
     /*
-        A method that obtains the number of r-permutations of a set with n elements 
-        based on whether repititions are allowed or not
-    */
+    A method that obtains the number of r-permutations of a set with n elements 
+    based on whether repititions are allowed or not
+     */
     public static int permute(int n, int r, boolean repititionsAllowed)
     {
         if (!isNegative(n, "permute") && !isNegative(r, "permute"))
-            if (n>r && !repititionsAllowed)
+            if (n>=r && !repititionsAllowed)
                 return factorial(n)/factorial(n-r);
             else if (n>r && repititionsAllowed)
                 return (int)Math.pow(n,r);
-        else 
+            else 
             {
-                System.out.println("Error: n is not greater than r");        
+                System.out.println("Error: n must not be than r");        
                 return -2;
             }
-    
+
         else
             return -1;
     }
+    
+    /*
+    A method that obtains the number of r-combinations of a set with n elements 
+    based on whether repititions are allowed or not
+     */
+    public static int combination(int n, int r, boolean repititionsAllowed)
+    {
+        if (!isNegative(n, "combination") && !isNegative(r, "combination"))
+            if (n>=r && !repititionsAllowed)
+                return factorial(n)/(factorial(r)*factorial(n-r));
+            else if (n>r && repititionsAllowed)
+                return factorial(n+r-1)/(factorial(r)*factorial(n-1));
+            else
+            {
+                System.out.println("Error: n must not be less than r");
+                return -2;
+            }
+        else
+            return -1;
+    }
+    
 }
